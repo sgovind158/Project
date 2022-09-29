871ead72f0764daec9930cf6fe8c609931b35041
sgovind158 <govindsahu2842@gmail.com>
2022-09-29 15:04:41 +0530

    form validation done

--- NodeE_Dashboard/frontend/src/Pages/AddProduct.jsx
+++ NodeE_Dashboard/frontend/src/Pages/AddProduct.jsx
@@ -6,11 +6,20 @@ const AddProduct = () => {
   const [brand ,setBrand] = useState("")
   const [price ,setPrice] = useState("")
   const [category ,setCategory] = useState("")
+  const [err,setErr] = useState(false)
   const navigate = useNavigate()
 
 
   const handleAddProductFun = async()=>{
     console.log(name,brand,price,category)
+
+
+
+    if(!name||!brand||!price||!category){
+    alert("please fill all detail ")
+    setErr(true)
+      return false
+    }
      const userId = JSON.parse(localStorage.getItem("user"))._id
     
      const data = await fetch("http://localhost:5000/addProduct",{
@@ -27,11 +36,20 @@ const AddProduct = () => {
   return (
     <div className={add.addProductDiv}>
      <h1>Add Product</h1>
-     <input type="text"  placeholder='product name' value={name} onChange={(e)=>setName(e.target.value)}/>
-     <input type="text" placeholder='product brand' value={brand} onChange={(e)=>setBrand(e.target.value)}/>
-     <input type="text" placeholder='product price' value={price} onChange={(e)=>setPrice(e.target.value)}/>
-     <input type="text" placeholder='product category' value={category} onChange={(e)=>setCategory(e.target.value)}/>
-     <button onClick={handleAddProductFun}>Add Product</button>
+     <input type="text" className={add.addInput}  placeholder='product name' value={name} onChange={(e)=>setName(e.target.value)}/>
+     { !name && <span className={add.addInvalid}>Please fill name </span>} 
+     {/* {<span className={add.addInvalid}>Please fill name </span>}   */}
+
+     <input type="text" className={add.addInput} placeholder='product brand' value={brand} onChange={(e)=>setBrand(e.target.value)}/>
+     { !brand && <span className={add.addInvalid}>Please fill brand </span>} 
+
+     <input type="text" className={add.addInput} placeholder='product price' value={price} onChange={(e)=>setPrice(e.target.value)}/>
+     { !price && <span className={add.addInvalid}>Please fill price </span>} 
+
+     <input type="text" className={add.addInput} placeholder='product category' value={category} onChange={(e)=>setCategory(e.target.value)}/>
+     { !category && <span className={add.addInvalid}>Please fill category </span>} 
+
+     <button className={add.button} onClick={handleAddProductFun}>Add Product</button>
     </div>
   )
 }
