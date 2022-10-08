bc98b0711119db777f0a80c47bf65c1ec42601ce
sgovind158 <govindsahu2842@gmail.com>
2022-10-08 12:38:45 +0530

    remove duplicate with typeScript

--- DSA21daychalange/day4.js
+++ DSA21daychalange/day4.js
@@ -1,24 +1,47 @@
-var plusOne = function (digits) {
-    var n = digits.length;
-    for (var i = n - 1; i >= 0; i--) {
-        if (digits[i] != 9) {
-            digits[i]++;
-            break;
+// var plusOne = function(digits:number[]):number[] {
+//     let n:number = digits.length ;
+//         for(let i = n-1 ; i>= 0; i--){
+//     if(digits[i] != 9){
+//         digits[i]++
+//                 break
+//             }else{
+//                 digits[i] = 0;
+//             }
+//         }
+//         if(digits[0] == 0){
+//             let res:number[] = []
+//             res.push(1)
+//             for(let i = 0; i<n;i++){
+//                 res.push(0)
+//             }
+//             return res 
+//         }
+//         return digits
+//     };
+//     var arr:number[] = [9,9,9,9]
+//     var ans = plusOne(arr)
+//     console.log(ans)
+/// in the worst case our arr run n time that wht T.C O(n)  
+// in the worst case we need extra arr that why our S.C O(n) 
+//arr  best case T.C    [1,2,3,4] T.C.O(1)
+//worst Case T.C     [9,9,9,9] T.C.O(n)
+//   best case S.C      [1,2,3,4] T.C.O(1)
+//  worst Case S.C    [9,9,9,9] T.C.O(n)
+//   --------------------------------------------------//
+// Two pointer tecnic 
+// remove duplicate 
+var removeDuplicate = function (arr, n) {
+    var j = 0;
+    for (var i = 0; i < n; i++) {
+        if (arr[i] != arr[i + 1]) {
+            arr[j++] = arr[i];
         }
-        else {
-            digits[i] = 0;
-        }
-    }
-    if (digits[0] == 0) {
-        var res = [];
-        res.push(1);
-        for (var i = 0; i < n; i++) {
-            res.push(0);
-        }
-        return res;
-    }
-    return digits;
+    } // loop end 
+    arr[j] = arr[n - 1];
+    return j;
 };
-var arr = [9, 9, 9, 9];
-var ans = plusOne(arr);
-console.log(ans);
+var arr = [1, 2, 2, 3, 3, 4, 4, 4, 5, 5];
+var m = removeDuplicate(arr, arr.length);
+for (var i = 0; i < m; i++) {
+    console.log(arr[i]);
+}
--- DSA21daychalange/day4.ts
+++ DSA21daychalange/day4.ts
@@ -1,31 +1,31 @@
-var plusOne = function(digits:number[]):number[] {
-    let n:number = digits.length ;
-        for(let i = n-1 ; i>= 0; i--){
-    if(digits[i] != 9){
-        digits[i]++
-                break
-            }else{
-                digits[i] = 0;
-            }
-        }
+// var plusOne = function(digits:number[]):number[] {
+//     let n:number = digits.length ;
+//         for(let i = n-1 ; i>= 0; i--){
+//     if(digits[i] != 9){
+//         digits[i]++
+//                 break
+//             }else{
+//                 digits[i] = 0;
+//             }
+//         }
         
-        if(digits[0] == 0){
-            let res:number[] = []
-            res.push(1)
-            for(let i = 0; i<n;i++){
-                res.push(0)
-            }
-            return res 
-        }
+//         if(digits[0] == 0){
+//             let res:number[] = []
+//             res.push(1)
+//             for(let i = 0; i<n;i++){
+//                 res.push(0)
+//             }
+//             return res 
+//         }
         
-        return digits
-    };
+//         return digits
+//     };
 
-    var arr:number[] = [9,9,9,9]
+//     var arr:number[] = [9,9,9,9]
 
-    var ans = plusOne(arr)
+//     var ans = plusOne(arr)
 
-    console.log(ans)
+//     console.log(ans)
 
 
     /// in the worst case our arr run n time that wht T.C O(n)  
@@ -34,3 +34,27 @@ var plusOne = function(digits:number[]):number[] {
      //worst Case T.C     [9,9,9,9] T.C.O(n)
    //   best case S.C      [1,2,3,4] T.C.O(1)
       //  worst Case S.C    [9,9,9,9] T.C.O(n)
+
+    //   --------------------------------------------------//
+
+// Two pointer tecnic 
+// remove duplicate 
+let removeDuplicate = (arr:number[],n:number):number=>{
+    let j:number  = 0;
+
+    for(let i:number = 0; i<n;i++){
+        if(arr[i] != arr[i+1]){
+            arr[j++] = arr[i]
+        }
+    }// loop end 
+
+    arr[j] = arr[n-1]
+    return j 
+}
+let arr:number[] = [1,2,2,3,3,4,4,4,5,5]
+let m:number =  removeDuplicate(arr,arr.length)
+for(let i:number = 0; i<m ;i++){
+    console.log(arr[i])
+}
+
+
